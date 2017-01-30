ae8fa6e48b666e481f19eee8d784c5e276f28d39
George C. Privon <gprivon@astro.puc.cl>
2017-01-30 13:46:57 -0300

    add LaTeX macro check to script

--- check_ms.sh
+++ check_ms.sh
@@ -3,3 +3,4 @@
 aspell -t check $1
 python /home/george/astro/software/Astronomy/PaperTools/punctuation_check.py $1
 python /home/george/astro/software/Astronomy/PaperTools/readability.py $1
+python /home/george/astro/software/Astronomy/PaperTools/macro_check.py $1
