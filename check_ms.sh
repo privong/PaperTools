b4750b5e6993d5999fb88f07730a87d8d8248bf7
George C. Privon <gprivon@astro.puc.cl>
2017-04-07 14:09:17 -0400

    add sha hashing of the file and flename

--- check_ms.sh
+++ check_ms.sh
@@ -3,3 +3,5 @@
 aspell -t check $1
 python /home/george/astro/software/Astronomy/PaperTools/punctuation_check.py $1
 python /home/george/astro/software/Astronomy/PaperTools/readability.py $1
+sha384sum $1
+echo $1 | sha1sum
