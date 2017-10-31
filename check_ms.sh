16d1c1bfcf8186e074141ec3f68d4b82def50020
George C. Privon <george.privon@ufl.edu>
2017-10-31 09:16:19 -0400

    more informative output

--- check_ms.sh
+++ check_ms.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 aspell -t check $1
+echo ""
 python /home/george/astro/software/Astronomy/PaperTools/punctuation_check.py $1
+echo ""
 python /home/george/astro/software/Astronomy/PaperTools/readability.py $1
+echo ""
 python /home/george/astro/software/Astronomy/PaperTools/macro_check.py $1
+echo ""
+echo "SHA384sum of the contents of $1:"
 sha384sum $1
+echo ""
+echo "SHA384sum of the filename $1:"
 echo $1 | sha1sum
