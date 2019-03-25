b650943c545b9b6e9edf1923b00f6f6271939135
George C. Privon <george.privon@ufl.edu>
2019-03-24 21:50:35 -0400

    it's bibtex

--- bib_update.py
+++ bib_update.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Load a bibtext file, find the arXiv entries within the past X years and
+Load a bibtex file, find the arXiv entries within the past X years and
 query ADS to see if the paper has been published. If so, update the bibtex
 entry.
 """
